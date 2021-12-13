d4dd70f7c0fbfeb88e37ef596040bf38eca6fa5d
Bruno Brum <brunobrum@gmail.com>
2021-12-13 12:06:15 -0500

    Add project files.

---
+++ Générateur XML/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WindowsFormsApp2
+{
+    public partial class frmXML : Form
+    {
+        public frmXML()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            bool verifierFichier = false;
+            verifierFichier = VerifierXMLExistant(verifierFichier);
+                       
+            if (verifierFichier == true) {
+
+                //Lire le fichier et charger les champs.
+
+                
+
+
+
+            }
+
+        }
+
+        public bool VerifierXMLExistant(bool exist)
+        {
+            // Vérifie si le fichier DB.xml existe sur l’ordinateur du client.
+
+            if (System.IO.File.Exists(@"C:\AudioClick\BaseTemp\DB.xml"))
+            {
+                exist = true;
+            }
+            else
+            {
+                exist = false;
+            }
+            return exist;
+        }
+
+
+
+    }
+}
